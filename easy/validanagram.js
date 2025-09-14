7ab29662b21c87e2bc8d94862ed7d143ca369ead
rayne <pothong.r@gmail.com>
2025-09-13 19:46:27 -0700

    added valid anagram

---
+++ easy/validanagram.js
@@ -0,0 +1,20 @@
+// Valid Anagram
+var isAnagram = function(s, t) {
+    if (s.length !== t.length){
+        return false;
+    }
+
+    const map = new Map();
+
+    for (let i = 0; i < s.length; i++){
+        map.set(s[i], (map.get(s[i]) || 0) + 1);
+        map.set(t[i], (map.get(t[i]) || 0) - 1);
+    }
+
+    for (let count of map.values()){
+        if (count !== 0){
+            return false;
+        }
+    }
+    return true;
+};
